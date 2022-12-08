3957014709060976ae89a37db7329bb4e8a81ab9
ChristofferHolmesland <christoffer.holmesland@gmail.com>
2022-12-08 16:51:35 +0100

    Day 6: Completed task 1

---
+++ day6.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+text = ''
+with open('day6.txt', 'r') as f:
+    text = f.read().strip()
+
+def task_one(text, num=4):
+    chars = deque(text[:num], maxlen=num)
+
+    if chars.count(chars[-1]) == 1:
+        return 4
+
+    for i in range(num, len(text)):
+        chars.append(text[i])
+
+        for j in range(num - 1, 0, -1):
+            if chars.count(chars[j]) != 1:
+                break
+        else:
+            return i + 1
+    
+    return -1
+
+print(task_one(text))
