ee8cad72e801fb1ccbe2fc2b781a3e6f5dcdcfc0
ChristofferHolmesland <christoffer.holmesland@gmail.com>
2022-12-08 17:53:32 +0100

    Day 6: Reduced memory usage by 25%

--- day6.py
+++ day6.py
@@ -7,13 +7,15 @@ with open('day6.txt', 'r') as f:
 def task_one(text, num=4):
     chars = deque(text[:num], maxlen=num)
 
+    previous_four = list(range(num - 1, 0, -1))
+
     if chars.count(chars[-1]) == 1:
         return 4
 
-    for i in range(num, len(text)):
+    for i in list(range(num, len(text))):
         chars.append(text[i])
 
-        for j in range(num - 1, 0, -1):
+        for j in previous_four:
             if chars.count(chars[j]) != 1:
                 break
         else:
